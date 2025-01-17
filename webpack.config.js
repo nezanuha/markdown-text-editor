9f610d41d7a2fa70a76e586a4246295262f42aca
nezanuha <dev@nezanuha.com>
2025-01-17 11:02:02 +0530

    Add production mode check and minify output for cdn

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,64 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    entry: {
-        "index":'./src/plugins/index.js',
-        "markdown-text-editor": './src/plugins/markdown/editor.js'
-    },
-    output: {
-        filename: '[name].js', // Output file name
-        path: path.resolve(__dirname, 'dist'), // Output directory
-        library: '[name]', // Name of the library
-        libraryTarget: 'umd', // Universal Module Definition
-        libraryExport: 'default', // Use default export
-        globalObject: 'this', // Ensures it works in Node.js and browser
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/, // Handle CSS files
-                use: [
-       
-                    'style-loader', // Inject CSS into the DOM
-                    'css-loader', // Translates CSS into CommonJS
-                    'postcss-loader', // Processes CSS with PostCSS
-                ],
-            },
-            {
-                test: /\.m?js$/, // Handle JavaScript files
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production';
+    return {
+        mode: isProd ? 'production' : 'development', // Set to 'production' for minified output
+        entry: {
+            "index":'./src/plugins/index.js',
+            "markdown-text-editor": './src/plugins/markdown/editor.js'
+        },
+        output: {
+            filename: '[name].min.js', // Output file name
+            path: path.resolve(__dirname, 'dist'), // Output directory
+            library: '[name]', // Name of the library
+            libraryTarget: 'umd', // Universal Module Definition
+            libraryExport: 'default', // Use default export
+            globalObject: 'this', // Ensures it works in Node.js and browser
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/, // Handle CSS files
+                    use: [
+        
+                        'style-loader', // Inject CSS into the DOM
+                        'css-loader', // Translates CSS into CommonJS
+                        'postcss-loader', // Processes CSS with PostCSS
+                    ],
+                },
+                {
+                    test: /\.m?js$/, // Handle JavaScript files
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
                     },
                 },
-            },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].min.css', // Output CSS filename
+            }),
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles.css', // Output CSS filename
-        }),
-    ],
-    mode: 'development', // Set to 'production' for minified output
-    devtool: 'source-map', // Enable source maps for easier debugging
+        devtool: isProd ? false : 'source-map', // Enable source maps for easier debugging
+        optimization: {
+            minimize: true, // Enable minimization
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: false, // Avoid creating separate LICENSE files
+                    terserOptions: {
+                        format: {
+                            comments: false, // Remove comments
+                        },
+                    },
+                }),
+            ],
+        }
+    }
 };
