24fcbe70e034744f8539776ad83e10d529763b66
Mohammed Rashad <rashadmehtab7@gmail.com>
2025-05-12 11:35:47 +0400

    Renamed selectors to avoid naming conflicts with other projects.

--- src/markdown/Toolbar/tools/LinkTool.js
+++ src/markdown/Toolbar/tools/LinkTool.js
@@ -34,7 +34,7 @@ class LinkTool extends MakeTool {
             const bodyHTML =`
                 <div class="flex justify-between items-center gap-3">
                     <div class="font-medium">Link</div>
-                    <button type="button" class="btn btn-ghost btn-xs btn-circle" onclick="toggleModal.remove()">
+                    <button type="button" class="btn btn-ghost btn-xs btn-circle" onclick="nezanuha_toggleModal.remove()">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
                     </button>
                 </div>
--- src/markdown/components/modal.js
+++ src/markdown/components/modal.js
@@ -1,23 +1,23 @@
 export function modal(event, size, bodyHTML) {
     const self = event.target;
     const mde = self.closest(".markdown-editor-wrapper");
-    if(mde.querySelector(".modal")){
-        mde.querySelector(".modal").remove(); 
+    if(mde.querySelector(".markdown-modal")){
+        mde.querySelector(".markdown-modal").remove(); 
     }
 
     const modalHTML = `
-        <dialog class="modal modal-y-top lg:modal-y-center modal-x-center" id="toggleModal">
+        <dialog class="modal modal-y-top lg:modal-y-center modal-x-center markdown-modal" id="nezanuha_toggleModal">
             <div class="modal-body ${size}">
                 ${bodyHTML}
             </div>
         </dialog>
     `;
 
-    if(!mde.querySelector(".modal")){
+    if(!mde.querySelector(".markdown-modal")){
         mde.insertAdjacentHTML("beforeend", modalHTML);
     }
     
-    const modal = mde.querySelector(".modal");
+    const modal = mde.querySelector(".markdown-modal");
     modal.showModal();
     return modal;
 }
